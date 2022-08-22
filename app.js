99a7f472ecaab42f11c011719ecd762d1a9a6be7
Natalya Maximova <natka-31@mail.ru>
2022-08-22 16:52:44 +0500

    исправить ошибку

--- app.js
+++ app.js
@@ -2,7 +2,6 @@ const express = require('express');
 
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
-const NotFoundError = require('./Error/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -21,16 +20,6 @@ app.use((req, res, next) => {
   };
 
   next();
-});
-
-app.use('*', (req, res) => {
-  try {
-    throw new NotFoundError('Запрашиваемая страница не найдена');
-  } catch (err) {
-    if (err instanceof NotFoundError) {
-      res.status(404).send({ message: err.message });
-    }
-  }
 });
 
 app.use('/', require('./routes/users'));
