9958e2478d4c92a15a2c01aae0ede8ef0d348d2e
Natalya Maximova <natka-31@mail.ru>
2022-08-30 15:30:26 +0500

    Добавьте email и password к схеме пользователя

--- models/user.js
+++ models/user.js
@@ -1,21 +1,37 @@
 const mongoose = require('mongoose');
+const isEmail = require('validator/lib/isEmail');
 
 const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    validate: {
+      validator: (v) => isEmail(v),
+      message: 'Формат почты указан не верно',
+    },
+    unique: true, // поле указывает, что почта должна быть уникальна
+    required: true,
+  },
+  password: {
+    type: String,
+    minlength: 8,
+    select: false, // поле выделяет область ввода объекта.
+    required: true,
+  },
   name: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Жак-Ив Кусто',
   },
   about: {
     type: String,
     minlength: 2,
     maxlength: 30,
-    required: true,
+    default: 'Исследователь',
   },
   avatar: {
     type: String,
-    required: true,
+    default: 'https://pictures.s3.yandex.net/resources/jacques-cousteau_1604399756.png',
   },
 });
 
