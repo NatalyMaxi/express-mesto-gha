9d45c926c878068ec1b8c75a5c03d60b6c49a1f7
Natalya Maximova <natka-31@mail.ru>
2022-08-30 16:26:12 +0500

    настроить helmet

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const helmet = require('helmet'); // помогает защитить приложение от некоторых широко известных веб-уязвимостей путем соответствующей настройки заголовков HTTP
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
@@ -7,6 +8,9 @@ const { ERROR_NOT_FOUND } = require('./utils/utils');
 
 const { PORT = 3000 } = process.env;
 const app = express();
+
+app.use(helmet());
+app.disable('x-powered-by'); // отключает заголовок X-Powered-By (заголовок обычно указывает платформу приложений, на которой работает сервер)
 
 mongoose.connect('mongodb://localhost:27017/mestodb ', {
   useNewUrlParser: true,
