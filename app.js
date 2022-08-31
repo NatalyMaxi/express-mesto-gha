1876d95835853cd5e625e9bd0d3b5147430ea63c
Natalya Maximova <natka-31@mail.ru>
2022-08-31 21:11:17 +0500

    удалить хардкорный мидлвэр

--- app.js
+++ app.js
@@ -22,14 +22,6 @@ mongoose.connect('mongodb://localhost:27017/mestodb ', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  req.user = {
-    _id: '6301183df3a6c9a1afcafbe5',
-  };
-
-  next();
-});
-
 app.post('/signin', login);
 app.post('/signup', createUser);
 
