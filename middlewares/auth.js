c1fb8c89363b79b6a02762ba7387f3f503d9c611
Natalya Maximova <natka-31@mail.ru>
2022-09-01 20:26:27 +0500

    отменить изменения

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,18 +1,20 @@
-require('dotenv').config();
-const jwt = require('jsonwebtoken');
+const jsonwebtoken = require('jsonwebtoken');
 const AuthorizationError = require('../Error/AuthorizationError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const token = req.cookies.jwt;
-  if (token == null) {
-    return next(new AuthorizationError('Необходимо авторизироваться'));
-  } let payload;
+  const { jwt } = req.cookies;
+  if (!jwt) {
+    return res
+      .status(401)
+      .send({ message: 'Необходимо авторизироваться' });
+  }
+  let payload;
   try {
-    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
+    payload = jsonwebtoken.verify(jwt, NODE_ENV ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    return next(new AuthorizationError('Необходимо авторизироваться'));
-  } req.user = payload;
+    next(new AuthorizationError({ message: 'Необходимо авторизироваться' }));
+  }req.user = payload;
   return next();
 };
