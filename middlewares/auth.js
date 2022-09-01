e17965f9385ead017b7f7c344e72bc701a62fef5
Natalya Maximova <natka-31@mail.ru>
2022-09-01 16:59:59 +0500

    изменить авторизацию

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,22 +1,17 @@
-const jsonwebtoken = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const AuthorizationError = require('../Error/AuthorizationError');
 
 const { NODE_ENV, JWT_SECRET } = process.env;
 
 module.exports = (req, res, next) => {
-  const { jwt } = req.cookies;
-  if (!jwt) {
-    return res
-      .status(401)
-      .send({ message: 'Необходимо авторизироваться' });
-  }
-  let payload;
+  const token = req.cookies.jwt;
+  if (token == null) {
+    return next(new AuthorizationError('Необходима авторизация'));
+  } let payload;
   try {
-    payload = jsonwebtoken.verify(jwt, NODE_ENV ? JWT_SECRET : 'dev-secret');
+    payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    next(new AuthorizationError({ message: 'Необходимо авторизироваться' }));
-  }
-  req.user = payload;
-  next();
-  return true;
+    return next(new AuthorizationError('Необходима авторизация'));
+  } req.user = payload;
+  return next();
 };
