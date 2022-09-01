a77dd41cfdba726150b3ab4caeeaebaa0dc9e7af
Natalya Maximova <natka-31@mail.ru>
2022-09-01 11:56:35 +0500

    добавить регулярное выражение для проверки аватара

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require('express');
 
+const { celebrate, Joi } = require('celebrate');
 const helmet = require('helmet'); // помогает защитить приложение от некоторых широко известных веб-уязвимостей путем соответствующей настройки заголовков HTTP
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const auth = require('./middlewares/auth');
 const { createUser, login } = require('./controllers/users');
 
-const { ERROR_NOT_FOUND } = require('./utils/utils');
+const NotFoundError = require('./Error/NotFoundError');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -22,8 +23,29 @@ mongoose.connect('mongodb://localhost:27017/mestodb ', {
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.post('/signin', login);
-app.post('/signup', createUser);
+app.post(
+  '/signup',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+      name: Joi.string().min(2).max(30),
+      about: Joi.string().min(2).max(30),
+      avatar: Joi.string().regex(/^((ftp|http|https):\/\/)?(www\.)?([A-Za-zА-Яа-я0-9]{1}[A-Za-zА-Яа-я0-9-]*\.?)*\.{1}[A-Za-zА-Яа-я0-9-]{2,8}(\/([\w#!:.?+=&%@!\-/])*)?/),
+    }),
+  }),
+  createUser,
+);
+app.post(
+  '/signin',
+  celebrate({
+    body: Joi.object().keys({
+      email: Joi.string().required().email(),
+      password: Joi.string().required(),
+    }),
+  }),
+  login,
+);
 
 app.use(auth); // защищает маршруты, которым нужны авторизация
 
@@ -31,8 +53,8 @@ app.use('/', require('./routes/users'));
 app.use('/', require('./routes/cards'));
 
 // Обработка запроса на несуществующий роут
-app.use((req, res) => {
-  res.status(ERROR_NOT_FOUND).send({ message: 'Запрашиваемая страница не найдена' });
+app.use((req, res, next) => {
+  next(new NotFoundError('Запрашиваемая страница не найдена'));
 });
 
 app.listen(PORT, () => {
