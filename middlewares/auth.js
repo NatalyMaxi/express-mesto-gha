56e96f9b6e254bfa9ae698490e74ea47e6154af0
Natalya Maximova <natka-31@mail.ru>
2022-09-01 13:29:14 +0500

    исправить авторизацию

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,13 +6,15 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 module.exports = (req, res, next) => {
   const { jwt } = req.cookies;
   if (!jwt) {
-    throw new AuthorizationError('Необходима авторизация.');
+    return res
+      .status(401)
+      .send({ message: 'Необходимо авторизироваться' });
   }
   let payload;
   try {
     payload = jsonwebtoken.verify(jwt, NODE_ENV ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new AuthorizationError('Необходима авторизация.');
+    next(new AuthorizationError({ message: 'Необходимо авторизироваться' }));
   }
   req.user = payload;
   next();
