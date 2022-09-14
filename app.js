5d746020577018add438f045324496b6e751aeb2
Natalya Maximova <natka-31@mail.ru>
2022-09-14 14:15:15 +0500

    добавить поддержку CORS и краш тест

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const helmet = require('helmet'); // помогает защитить приложение от некоторых широко известных веб-уязвимостей путем соответствующей настройки заголовков HTTP
 const mongoose = require('mongoose');
@@ -17,11 +18,32 @@ const {
 const NotFoundError = require('./Error/NotFoundError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
+const options = {
+  origin: {
+    origin: [ // Массив доменов, с которых разрешены кросс-доменные запросы.
+      'http://localhost:3000',
+      'https://domainname.nataly.nomoredomains.sbs',
+      'https://praktikum.tk',
+      'http://praktikum.tk',
+      'localhost:3000',
+    ],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE', // настраивает заголовок CORS Access-Control-Allow-Methods.
+    preflightContinue: false, // передать предварительный ответ CORS следующему обработчику.
+    optionsSuccessStatus: 204, // Предоставляет код состояния для успешных OPTIONSзапросов.
+  },
+  credentials: true, // сообщает браузерам, следует ли предоставлять ответ внешнему коду JS.
+};
+
 const { PORT = 3000 } = process.env;
+
 const app = express();
+
+app.use('*', cors(options));
+
 app.use(cookieParser());
 
 app.use(helmet());
+
 app.disable('x-powered-by'); // отключает заголовок X-Powered-By (заголовок обычно указывает платформу приложений, на которой работает сервер)
 
 mongoose.connect('mongodb://localhost:27017/mestodb ', {
@@ -30,9 +52,17 @@ mongoose.connect('mongodb://localhost:27017/mestodb ', {
 });
 
 app.use(bodyParser.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(requestLogger); // подключаем логгер запросов
+
+// для проверки ревьювером, что сервер падает и Pm2 должен его восстановить
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+}); // Не забыть удалить этот код после успешного прохождения ревью
 
 app.post('/signin', validationLogin, login);
 
@@ -41,6 +71,7 @@ app.post('/signup', validationCreateUser, createUser);
 app.use(auth); // защищает маршруты, которым нужны авторизация
 
 app.use('/users', require('./routes/users'));
+
 app.use('/cards', require('./routes/cards'));
 
 // Обработка запроса на несуществующий роут
@@ -52,6 +83,7 @@ app.use((req, res, next) => {
 app.use(errorLogger); // подключаем логгер ошибок
 
 app.use(errors()); // обработчик ошибок celebrate
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
